c683b267c1983e44bd18bc8b1161ba36bf5ff40a
nmates-ocrolus <nmates@ocrolus.com>
2026-04-30 09:59:52 -0500

    Update Snowflake session connection method
    Replace get_active_session with st.connection to establish a Snowflake connection.

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages.
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -13,7 +13,9 @@ st.write(
 
 name_on_order = st.text_input('Name on Smoothie:')
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
