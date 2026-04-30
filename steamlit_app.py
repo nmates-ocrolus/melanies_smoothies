68ecbab88ccb43aa5c72eaea53d9e07f8566ba7a
nmates-ocrolus <nmates@ocrolus.com>
2026-04-30 10:23:16 -0500

    Integrate Smoothiefroot API for watermelon data
    Added functionality to fetch and display watermelon data from Smoothiefroot API.

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests  
+
 
 
 # Write directly to the app.
@@ -31,6 +33,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' ' 
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -45,7 +49,3 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,' + name_on_order + '!', icon="✅")
 
         
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
